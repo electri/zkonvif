66ff234f177eea5b62317ad4a221b0dcded443c9
sunkw <sunkwei@gmail.com>
2014-06-03 17:30:46 +0800

    add docs

--- libzkonvif/include/zk_wsdd.h
+++ libzkonvif/include/zk_wsdd.h
@@ -1,6 +1,17 @@
 /** zonekey onvif lib
 
   	zk_wsdd: 提供统一的设备发现，声明接口
+
+		target:
+			zk_wsdd_TargetDesc desc = {
+				"urn:xxxx-xxx....",
+				"eval",
+				"rtmp://xxxxx",
+			}
+
+			zk_wsdd_target_t *target = zk_wsdd_target_open(&desc);
+			....
+
  */
 
 #pragma once
@@ -14,12 +25,15 @@ extern "C" {
 
 	struct zk_wsdd_TargetDesc
 	{
-		const char *type; // 目前支持 'eval', 'ptz'
+		const char *id;		// 唯一标识，调用者应该记住 ...
+		const char *type; 	// 目前支持 'eval', 'ptz'
 		const char *url;	// 服务访问 url ...
 	};
 
 	zk_wsdd_target_t *zk_wsdd_target_open(const struct zk_wsdd_TargetDesc *desc);
 	void zk_wsdd_target_close(zk_wsdd_target_t *target);
+
+	
 
 #ifndef __cplusplus
 }
