3eac94974708929bbcc4ce809c08d8cbead8a110
sunkw <sunkwei@gmail.com>
2014-07-03 12:09:56 +0800

    test_cs_client: 用于测试 ptz, event 接口的 c# 程序

---
+++ build/windows/test_cs_client/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_cs_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dm_url = "http://localhost:10000";
+            if (args.Length > 0)
+                dm_url = args[0];
+
+            zonvif_dm.DeviceBinding dm = new zonvif_dm.DeviceBinding();
+            dm.Url = dm_url;
+
+            zonvif_dm.Service[] services = dm.GetServices(false);
+            foreach (zonvif_dm.Service s in services) {
+                string url = s.XAddr;
+                string ns = s.Namespace;
+                if (url != null && ns != null) {
+                    Console.WriteLine("INFO: Service '" + s.Namespace + "', url='" + url + "'");
+
+                    if (ns == "ptz")
+                        test_ptz(url);
+                    if (ns == "event")
+                        test_event(url);
+
+                }
+            }
+        }
+
+        static void test_ptz(string url)
+        {
+
+        }
+
+        static void test_event(string url)
+        {
+
+        }
+    }
+}
