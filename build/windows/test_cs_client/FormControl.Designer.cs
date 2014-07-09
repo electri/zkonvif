b576fd988c1de4c7ec6191fc1e8f1fb21c293609
liuguangyuan <liuguangyuan0532@gmail.com>
2014-07-09 14:40:49 +0800

    添加 form

---
+++ build/windows/test_cs_client/FormControl.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace test_cs_client
+{
+    partial class FormControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelIp = new System.Windows.Forms.Label();
+            this.comboBoxIp = new System.Windows.Forms.ComboBox();
+            this.labelPort = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.comboBoxPort = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // labelIp
+            // 
+            this.labelIp.AutoSize = true;
+            this.labelIp.Location = new System.Drawing.Point(29, 41);
+            this.labelIp.Name = "labelIp";
+            this.labelIp.Size = new System.Drawing.Size(47, 12);
+            this.labelIp.TabIndex = 0;
+            this.labelIp.Text = "ip地址:";
+            // 
+            // comboBoxIp
+            // 
+            this.comboBoxIp.FormattingEnabled = true;
+            this.comboBoxIp.Location = new System.Drawing.Point(82, 33);
+            this.comboBoxIp.Name = "comboBoxIp";
+            this.comboBoxIp.Size = new System.Drawing.Size(167, 20);
+            this.comboBoxIp.TabIndex = 1;
+            // 
+            // labelPort
+            // 
+            this.labelPort.AutoSize = true;
+            this.labelPort.Location = new System.Drawing.Point(271, 41);
+            this.labelPort.Name = "labelPort";
+            this.labelPort.Size = new System.Drawing.Size(47, 12);
+            this.labelPort.TabIndex = 2;
+            this.labelPort.Text = "端口号:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(742, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // comboBoxPort
+            // 
+            this.comboBoxPort.FormattingEnabled = true;
+            this.comboBoxPort.Location = new System.Drawing.Point(337, 33);
+            this.comboBoxPort.Name = "comboBoxPort";
+            this.comboBoxPort.Size = new System.Drawing.Size(121, 20);
+            this.comboBoxPort.TabIndex = 4;
+            // 
+            // FormControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 558);
+            this.Controls.Add(this.comboBoxPort);
+            this.Controls.Add(this.labelPort);
+            this.Controls.Add(this.comboBoxIp);
+            this.Controls.Add(this.labelIp);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormControl";
+            this.Text = "FormControl";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelIp;
+        private System.Windows.Forms.ComboBox comboBoxIp;
+        private System.Windows.Forms.Label labelPort;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ComboBox comboBoxPort;
+    }
+}
--- build/windows/test_cs_client/Program.cs
+++ build/windows/test_cs_client/Program.cs
@@ -5,154 +5,44 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Windows.Forms;
 
 namespace test_cs_client
 {
+            /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
     class Program
     {
+         [STAThread]
         static void Main(string[] args)
         {
-            string dm_url = "http://172.16.1.111:10000";
-            if (args.Length > 0)
-                dm_url = args[0];
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormControl());
 
-            zonvif_dm.DeviceBinding dm = new zonvif_dm.DeviceBinding();
-            dm.Url = dm_url;
+            //string dm_url = "http://172.16.1.111:10000";
+            //if (args.Length > 0)
+            //    dm_url = args[0];
 
-            zonvif_dm.Service[] services = dm.GetServices(false);
-            foreach (zonvif_dm.Service s in services) {
-                string url = s.XAddr;
-                string ns = s.Namespace;
-                if (url != null && ns != null) {
-                    Console.WriteLine("INFO: Service '" + s.Namespace + "', url='" + url + "'");
+            //zonvif_dm.DeviceBinding dm = new zonvif_dm.DeviceBinding();
+            //dm.Url = dm_url;
 
-                    if (ns == "ptz")
-                        test_ptz(url);
-                    else if (ns == "event")
-                        test_event(url);
-                    else
-                        Console.WriteLine("WARNING: Unknown ns '" + ns + "'!");
-                }
-            }
-        }
+            //zonvif_dm.Service[] services = dm.GetServices(false);
+            //foreach (zonvif_dm.Service s in services) {
+            //    string url = s.XAddr;
+            //    string ns = s.Namespace;
+            //    if (url != null && ns != null) {
+            //        Console.WriteLine("INFO: Service '" + s.Namespace + "', url='" + url + "'");
 
-        static void test_ptz(string url)
-        {
-            Console.WriteLine("===== test ptz interface =====");
-
-            zonvif_ptz.PTZBinding ptz = new zonvif_ptz.PTZBinding();
-            ptz.Url = url;
-
-            // GetNodes
-            zonvif_ptz.PTZNode []nodes = ptz.GetNodes();
-            Console.WriteLine(string.Format("There are {0} ptzs", nodes.Length));
-            foreach (zonvif_ptz.PTZNode node in nodes) {
-                zonvif_ptz.PTZNode n = ptz.GetNode(node.token);
-                test_ptz_AbsoluteMove(ptz, n);
-                //test_ptz_move(ptz, n);
-            }
-
-            Console.WriteLine("------------ end -------------");
-        }
-
-        static void test_ptz_move(zonvif_ptz.PTZBinding ptz, zonvif_ptz.PTZNode node)
-        {
-            Console.WriteLine(string.Format("INFO: testing ptz move"));
-            zonvif_ptz.PTZSpeed speed = new zonvif_ptz.PTZSpeed();
-            speed.PanTilt = new zonvif_ptz.Vector2D();
-
-            speed.Zoom = new zonvif_ptz.Vector1D();
-            speed.Zoom.x = (float)7.0;
-            long timeout = 1000;
-            TimeSpan ts = new TimeSpan(timeout);
-
-            speed.PanTilt.x = 40;
-            speed.PanTilt.y = 0;
-            ptz.ContinuousMove(node.token, speed, SoapDuration.ToString(ts));
-            System.Threading.Thread.Sleep(200);
-            ptz.Stop(node.token, false, false, false, false);
-
-            System.Threading.Thread.Sleep(200);
-
-            speed.PanTilt.x = -40;
-            speed.PanTilt.y = 0;
-            ptz.ContinuousMove(node.token, speed, SoapDuration.ToString(ts));
-            System.Threading.Thread.Sleep(200);
-            ptz.Stop(node.token, false, false, false, false);
-
-            System.Threading.Thread.Sleep(200);
-
-            speed.PanTilt.x = 0;
-            speed.PanTilt.y = 40;
-            ptz.ContinuousMove(node.token, speed, SoapDuration.ToString(ts));
-            System.Threading.Thread.Sleep(200);
-            ptz.Stop(node.token, false, false, false, false);
-
-            System.Threading.Thread.Sleep(200);
-
-            speed.PanTilt.x = 0;
-            speed.PanTilt.y = -40;
-            ptz.ContinuousMove(node.token, speed, SoapDuration.ToString(ts));
-            System.Threading.Thread.Sleep(200);
-            ptz.Stop(node.token, false, false, false, false);
-            System.Threading.Thread.Sleep(200);
-        }
-
-        static void test_ptz_AbsoluteMove(zonvif_ptz.PTZBinding ptz, zonvif_ptz.PTZNode node)
-        {
-            Console.WriteLine(string.Format("INFO: testing ptz: token '{0}', name '{1}'", node.token, node.Name));
-
-            double scale = ptz.GetScales(node.token);
-            /*
-            // Configuration 
-            zonvif_ptz.PTZConfiguration cfg = ptz.GetConfiguration(node.token);
-            Console.WriteLine(string.Format("\tConfiguration: of token '{0}'", cfg.NodeToken));
-            if (cfg.DefaultPTZSpeed != null) {
-                Console.WriteLine(string.Format("\t\tDefaultPTZSpeed: {0}, {1}, {2}", cfg.DefaultPTZSpeed.PanTilt.x, 
-                    cfg.DefaultPTZSpeed.PanTilt.y, cfg.DefaultPTZSpeed.Zoom.x));
-            }
-            if (cfg.DefaultAbsolutePantTiltPositionSpace != null) {
-                Console.WriteLine(string.Format("\t\tDefaultAbsolutePanTiltPositionSpace: {0}", cfg.DefaultAbsolutePantTiltPositionSpace));
-            }
-             */
-
-            zonvif_ptz.PTZSpeed speed = new zonvif_ptz.PTZSpeed();
-            speed.PanTilt = new zonvif_ptz.Vector2D();
-            speed.PanTilt.x = 10;
-            speed.PanTilt.y = 10;
-            speed.Zoom = new zonvif_ptz.Vector1D();
-            speed.Zoom.x = 1;
-
-            zonvif_ptz.PTZVector pos = new zonvif_ptz.PTZVector();
-            pos.PanTilt = new zonvif_ptz.Vector2D();
-            pos.PanTilt.x = (float)300.0;
-            pos.PanTilt.y = (float)300.0;
-
-            // 设置云台指向：
-            ptz.AbsoluteMove(node.token, pos, speed);
-            System.Threading.Thread.Sleep(200);
-            //Console.WriteLine("Scale : {0}", scale.ToString());
-            zonvif_ptz.ZonekeyPtzParamType ptzParams = ptz.GetPtzParams(node.token);
-            Console.WriteLine(string.Format("PtzParams: {0}, {1}, {2}, {3}, {4}", ptzParams.f,
-                ptzParams.pan_max_va, ptzParams.pan_min_angle, ptzParams.tilt_max_va, ptzParams.tilt_min_angle));
-
-            // 连续数次调用 ...
-            for (int i = 0; i < 3; i++)
-            {
-                zonvif_ptz.PTZStatus status = ptz.GetStatus(node.token);
-                Console.WriteLine(string.Format("\t\t[#{0}]: pos={1}, {2}, {3}", i, status.Position.PanTilt.x, status.Position.PanTilt.y, status.Position.Zoom.x));
-
-                System.Threading.Thread.Sleep(100);
-            }
-        }
-
-        static void test_event(string url)
-        {
-            Console.WriteLine("==== test event interface ====");
-
-            /// TODO: 测试事件接口 ...
-
-            Console.WriteLine("------------ end -------------");
+            //        if (ns == "ptz")
+            //            test_ptz(url);
+            //        else if (ns == "event")
+            //            test_event(url);
+            //        else
+            //            Console.WriteLine("WARNING: Unknown ns '" + ns + "'!");
+            //    }
+           // }
         }
     }
 }
