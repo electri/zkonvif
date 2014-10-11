908334a73d917d820a6df10bb6bd6a44153b4d35
liuguangyuan <liuguangyuan0532@gmail.com>
2014-10-11 17:51:16 +0800

    添加sms(服务管理服务)

--- zkdm/python/zkdm-setup/zkdm/ptz/install_ptz_winservice.bat
+++ zkdm/python/zkdm-setup/zkdm/ptz/install_ptz_winservice.bat
@@ -12,9 +12,8 @@ sc config zonekey.service.ptz start= auto
 @echo **********************************************************************
 @echo *                       注意事项                                      *
 @echo * 在windows services中,查看 zonekey.service.ptz 是否存在? 若存在:     *
-@echo *   1  在c:\zkdm\ptz目录下,修改配置文件teacher.config和student.config *
-@echo *   (hva,vva分别为水平视角,垂直视角,以度为单位)                       *
-@echo *   2 启动 zonekey.service.ptz  服务                                  *
+@echo *   1 在c:\zkdm\ptz目录下,修改配置文件teacher.config和student.config *
+@echo *   2 启动zonekey.service.ptz服务 或者 重启计算机                                 *
 @echo **********************************************************************
 @echo.
 @echo off
