d3c4776b3e7ccdc330ec17a113b94cf999f1e6fb
jp100hdv2 <sunkwei@gmail.com>
2014-11-28 12:46:18 +0800

    ..

--- zkdm/python/zkdm/dm/install_dm_winservice.bat
+++ zkdm/python/zkdm/dm/install_dm_winservice.bat
@@ -1,7 +1,9 @@
 @echo on
 
-@c:\Python27\python DMServer.py
+@c:\Python27\python DMWinService.py install
 
+@sc config zonekey.service.dm start= auto
+@sc start zonekey.service.dm
 @echo zonekey.service.dm succeed and running ...
 @echo off
 
