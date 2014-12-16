c9f5f8b2dca4426e271c1899404c3508246f0ddc
sunkw <sunkwei@gmail.com>
2014-12-16 10:59:02 +0800

    reght: 支持多服务聚合注册/心跳

--- zkdm/python/zkdm/common/reght.py
+++ zkdm/python/zkdm/common/reght.py
@@ -8,62 +8,109 @@
 #
 #################################################################
 
-import urllib2, sys, json, io, time, threading
+import urllib2, sys, json, io, time, threading, logging
 from utils import zkutils
 
-class RegHt(threading.Thread):
-    ''' 注册/注销/心跳类, 本质是一个工作现场, 实现周期心跳 ...
-            '''
-    def __init__(self, service_type, service_id, service_url, mgrt_baseurl = None):
-        ''' 初始化并启动工作线程, 直到调用 join 结束
-            mgrt_baseurl 为名字服务的基础 url, 如 http://192.168.1.100:8080/deviceService
+class GroupOfServices:
+    ''' 实现一个生成器，每次 next() 就执行一次注册/心跳
+        如果服务太多，每隔10秒，连续注册/心跳，会导致网络剧烈抖动，
+        因此这里简单的划分为 10 个组，然后每个 1秒执行一组 ....
+    '''
+    def __init__(self, services_desc):
+        self.__10b = [ [], [], [], [], [], [], [], [], [], [] ] # 保存需要注册的
+        self.__10bht = [ [], [], [], [], [], [], [], [], [], [] ] # 保存需要心跳的
+        self.__distribution(services_desc)
+
+
+    def __distribution(self, sds):
+        ''' 将服务列表，平均分配到 __10b 中 '''
+        i = 0
+        for sd in sds:
+            self.__10b[i].append(sd)
+            i += 1;
+            i %= len(self.__10b)
+
+
+    def reg(self, regop):
+        ''' 对下一组，执行注册 
+            如果都注册成功了，则不做啥操作 ...
         '''
-        self._service_type = service_type
-        self._service_id = service_id
-        self._quit = False
-        self._mgrt_baseurl = mgrt_baseurl
-        self._quit_notify = threading.Event()
-        self._myip = zkutils().myip_real()
-        self._mymac = zkutils().mymac()
-        self.baseurl = 'http://%s:'%(self._myip)
-        self._service_url = self.baseurl + service_url 
-        threading.Thread.__init__(self)
-        self.daemon = True # 因为有心跳, 不调用 unreg() 也是安全的
-        self.start()    # 启动工作线程
-
-    def join(self):
-        ''' 主动结束 '''
-        self._quit = True
-        self._quit_notify.set()
-        threading.Thread.join(self)     # 等待工作线程结束
+        i = 0
+        while True:
+            i %= len(self.__10b)
+            self.__reg(regop, self.__10b[i], self.__10bht[i])
+            i += 1
+            yield
 
 
-    def run(self):
-        ''' step:
-                1. 注册,直到成功
-                2. 每隔10秒, 心跳
-                3. 随时处理退出 ...
-        '''
-        if not self._mgrt_baseurl:
-            try:
-                self._mgrt_baseurl = self._load_mgrt_baseurl()
-            except Exception as e:
-                print "error"
-                print "\t" + e.message
-                print "\t" + sys._getframe().f_code.co_filename
-                print "\t" + str(sys._getframe().f_lineno - 5) + 'line'
-                self._mgrt_baseurl = r'http://127.0.0.1:8080/deviceService/' 
-        while not self._reg() and not self._quit:
-            self._quit_notify.wait(5.000)
-        while self._hb and not self._quit:
-            self._quit_notify.wait(10.0)
-        self._unreg()
+    def __reg(self, op, breg, bht):
+        ''' 对 breg 中的进行注册，成功，就从 breg 中删除，并且保持到 bht 中 '''
+        for sd in breg:
+            if op(sd):
+                breg.remove(sd)
+                bht.append(sd)
 
 
-    def _log(self, info):
-        print info
+    def ht(self, htop):
+        ''' 对下一组执行心跳 '''
+        i = 0
+        while True:
+            i %= len(self.__10bht)
+            for sd in self.__10bht[i]:
+                htop(sd)
+            i += 1
+            yield
 
-    def _load_mgrt_baseurl(self):
+
+class RegHtOper:
+    def __init__(self, mgrt_base_url, ip, mac):
+        if mgrt_base_url is None:
+            mgrt_base_url = self.__load_base_url()
+        self.__mgrt_base_url = mgrt_base_url
+        self.__ip = ip
+        self.__mac = mac
+
+    def __get_utf8_body(self, req):
+        # FIXME: 更合理的应该是解析 Content-Type ...
+        body = '';
+        b = req.read().decode('utf-8')
+        while b:
+            body += b
+            b = req.read().decode('utf-8')
+        return body
+
+    def regop(self, sd):
+        url = self.__mgrt_base_url + 'registering?serviceinfo=%s_%s_%s_%s_%s' % \
+              (self.__ip, self.__mac, sd['type'], sd['id'], sd['url'])
+        try:
+            req = urllib2.urlopen(url)
+            body = self.__get_utf8_body(req)
+            ret = json.loads(body)
+        except:
+            print 'regop: exception, url=', url
+            return False
+
+        if u'已经注册' not in ret['info']:
+            return False
+        return True
+
+    def htop(self, sd):
+        url = self.__mgrt_base_url + 'heartbeat?serviceinfo=%s_%s_%s_%s' % \
+              (self.__ip, self.__mac, sd['type'], sd['id'])
+        try:
+            req = urllib2.urlopen(url)
+            body = self.__get_utf8_body(req)
+            ret = json.loads(body)
+        except:
+            print 'htop: exception, url=', url
+            return False
+
+        if u'发送的心跳数据' in ret['info']:
+            return True
+        else:
+            return False
+
+    def __load_base_url(self):
         # TODO: 从配置中读取 ..
     	ret = json.load(io.open(r'../host/config.json', 'r', encoding='utf-8'))
         r = ret['regHbService']
@@ -74,69 +121,52 @@ class RegHt(threading.Thread):
 
         return 'http://%s:%s/deviceService/'%(r['sip'],r['sport'])
 
-    def _get_utf8_body(self, req):
-        # FIXME: 更合理的应该是解析 Content-Type ...
-        body = '';
-        b = req.read().decode('utf-8')
-        while b:
-            body += b
-            b = req.read().decode('utf-8')
-        return body
 
-    def _reg(self):
-        url = self._load_mgrt_baseurl() + 'registering?serviceinfo=%s_%s_%s_%s_%s' % \
-              (self._myip, self._mymac, self._service_type, self._service_id, self._service_url)
-#self._log("_reg: using url: " + url)
-        req = None
-        try:
-            req = urllib2.urlopen(url)
-#except urllib2.HTTPError:
-#            self._log('\tHTTPError: ')
-#            return False
-#        except urllib2.URLError:
-#            self._log('\tURLError: ')
-#            return False
-        except:
-#    self._log('\treg return webpage failure')
-            return False
-        s = self._get_utf8_body(req)
-        # TODO: 这里解析注册返回的 json ...
-        ret = {}
-        try:
-            ret.update(json.loads(s))
-        except:
-            return False
-        
-        if u'已经注册' not in ret['info']:
-            print ret['info']
-            return False
+class RegHt(threading.Thread):
+    ''' 注册一组服务，sds 为列表，每个 sd 为：
+            { 'type': service_type, 'id': service_id, 'url': service_url }
+    '''
+    def __init__(self, sds, mgrt_baseurl = None):
+        self.__quit = False
+        self.__quit_notify = threading.Event()
+        self.__mgrt_base_url = mgrt_baseurl
+        self.__sds = sds
+        threading.Thread.__init__(self)
+        self.daemon = True # 因为有心跳，不调用 unreg()，也是安全的
+        self.start()
 
-        return True
+    def join(self):
+        self.__quit = True
+        self.__quit_notify.set()
+        threading.Thread.join(self)
 
-    def _hb(self):
-        # TODO:
-        url = self._load_mgrt_baseurl() + 'heartbeat?serviceinfo=%s_%s_%s_%s' % \
-              (self._myip, self._mymac, self._service_type, self._service_id)
-        f = urllib2.urlopen(url)
-        s = self._get_utf8_body(f)
-        ret = json.loads(s)
-        if u'发送的心跳数据' in ret['info']:
-            self._log("_hb: url=" + url)
-            return True
-        else:
-            self._log('_hb:url=' + url + 'faile')
-            return False
-                 
-    def _unreg(self):
-        # TODO: url = self._load_mgrt_baseurl() + '?????'
-        # fuchun zhang can't support unreging a certainly service 
-        self._log("unreg  ...")
-'''
+    def run(self):
+        ip = zkutils().myip_real()
+        mac = zkutils().mymac()
+
+        oper = RegHtOper(self.__mgrt_base_url, ip, mac)
+        gos = GroupOfServices(self.__sds)
+
+        regfunc = gos.reg(oper.regop)
+        htfunc = gos.ht(oper.htop)
+
+        while not self.__quit:
+            if self.__quit_notify.wait(1.0):
+                continue
+            next(regfunc)
+            next(htfunc)
+
+
+
+sds = [ { 'type':'test', 'id':'1', 'url':'test://172.16.1.103:11111' },
+        { 'type':'test', 'id':'2', 'url':'test://172.16.1.103:11111' },
+        { 'type':'test', 'id':'3', 'url':'test://172.16.1.103:11111' },
+        { 'type':'test', 'id':'4', 'url':'test://172.16.1.103:11111' },
+      ]
+
+
 if __name__ == '__main__':
-    th = RegHt('', 'test-id', 'test://...')
-    time.sleep(100)
-    th.join()
-'''
-
-# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+    rh = RegHt(sds)
+    time.sleep(30.0);
+    rh.join()
 
