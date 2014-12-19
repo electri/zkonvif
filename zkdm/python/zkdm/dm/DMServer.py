d8fc0a2042a871bdd9b3b4110770ad5b2f954c56
sunkw <sunkwei@gmail.com>
2014-12-19 12:10:30 +0800

    DMServer: 支持 主机注册

--- zkdm/python/zkdm/dm/DMServer.py
+++ zkdm/python/zkdm/dm/DMServer.py
@@ -8,7 +8,12 @@ import platform
 sys.path.append('../')
 from common.reght import RegHt
 sys.path.append('../host')
+from utils import zkutils
 import Stat
+
+_myip = zkutils().myip_real()
+_mac = zkutils().mymac()
+
 
 # DM Service 端口
 DMS_PORT = 10000
@@ -17,249 +22,253 @@ _sm = None # 在 main 中创建
 plat = platform.uname()[0]
 
 class HelpHandler(RequestHandler):
-	''' 提示信息 ....
-	'''
-	def get(self):
-		self.render('./help.html')
+    ''' 提示信息 ....
+    '''
+    def get(self):
+        self.render('./help.html')
 
 
 class ServiceHandler(RequestHandler):
-	''' 处理服务的控制命令 ....
-		/dm/<service name>/<operator>?<params ...>
-	'''
-	def get(self, name, oper):
-		rc = {}
-		rc['result'] = 'ok'
-		rc['info'] = ''
+    ''' 处理服务的控制命令 ....
+        /dm/<service name>/<operator>?<params ...>
+    '''
+    def get(self, name, oper):
+        rc = {}
+        rc['result'] = 'ok'
+        rc['info'] = ''
 
-		if oper == 'start':
-			if _sm.start_service(name):
-				rc['info'] = 'started'
-			else:
-			 	rc['result'] = 'err'
-			 	rc['info'] = 'not found or disabled'
-		elif oper == 'stop':
-			if _sm.stop_service(name):
-				rc['info'] = 'stopped'
-			else:
-			 	rc['result'] = 'err'
-			 	rc['info'] = 'not found or disabled'
-		elif oper == 'disable':
-			_sm.enable_service(name, False)
-			rc['info'] = 'disabled'
-		elif oper == 'enable':
-			_sm.enable_service(name, True)
-			rc['info'] = 'enabled'
-		else:
-			rc['info'] = 'unknown operator'
-		
-		self.write(rc)
+        if oper == 'start':
+            if _sm.start_service(name):
+                rc['info'] = 'started'
+            else:
+                rc['result'] = 'err'
+                rc['info'] = 'not found or disabled'
+        elif oper == 'stop':
+            if _sm.stop_service(name):
+                rc['info'] = 'stopped'
+            else:
+                 rc['result'] = 'err'
+                 rc['info'] = 'not found or disabled'
+        elif oper == 'disable':
+            _sm.enable_service(name, False)
+            rc['info'] = 'disabled'
+        elif oper == 'enable':
+            _sm.enable_service(name, True)
+            rc['info'] = 'enabled'
+        else:
+            rc['info'] = 'unknown operator'
+        
+        self.write(rc)
 
 
 
 class ListServiceHandler(RequestHandler):
-	''' 返回服务列表 '''
-	def get(self):
-		rc = {}
-		rc['result'] = 'ok'
-		rc['info'] = ''
+    ''' 返回服务列表 '''
+    def get(self):
+        rc = {}
+        rc['result'] = 'ok'
+        rc['info'] = ''
 
-		ss = _sm.list_services_new()
-		value = {}
-		value['type'] = 'list'
-		value['data'] = ss
+        ss = _sm.list_services_new()
+        value = {}
+        value['type'] = 'list'
+        value['data'] = ss
 
-		rc['value'] = value
+        rc['value'] = value
 
-		self.write(rc)
+        self.write(rc)
 
 # 全局，用于主动结束 ...
 _ioloop = IOLoop.instance()
 pm = Stat.PerformanceMonitor()
-pm.start()
-rh = RegHt([ {'type':'dm', 'id':'dm', r'http://<ip>:10000/dm'}])
+#pm.start()
+rh = RegHt([ {'type':'dm', 'id':'dm', 'url':r'http://<ip>:10000/dm'}])
 
 class InternalHandler(RequestHandler):
-	def get(self):
-		rc = {}
-		rc['result'] = 'ok'
-		rc['info'] = ''
+    def get(self):
+        rc = {}
+        rc['result'] = 'ok'
+        rc['info'] = ''
 
-		command = self.get_argument('command', 'nothing')
-		if command == 'exit':
-			rc['info'] = 'exit!!!'
-			self.write(rc)
-			rh.join()
-			global _ioloop
-			_ioloop.stop()
-		elif command == 'version':
-			rc['info'] = 'now, not supported!!!'
-			rc['result'] = 'error'
-			self.write(rc)
+        command = self.get_argument('command', 'nothing')
+        if command == 'exit':
+            rc['info'] = 'exit!!!'
+            self.write(rc)
+            rh.join()
+            global _ioloop
+            _ioloop.stop()
+        elif command == 'version':
+            rc['info'] = 'now, not supported!!!'
+            rc['result'] = 'error'
+            self.write(rc)
 
 class HostHandler(RequestHandler):
-	''' 返回主机类型 
-	'''
-	def get(self):
-		rc = {}
-		rc['result'] = 'ok'
-		rc['info'] = ''
-		command = self.get_argument('command', 'nothing')
-		if command == 'type':
-			try:
-				f = io.open(r'../host/config.json', 'r', encoding='utf-8')
-				s = json.load(f)
-				rc['value'] = {}
-				rc['value']['type'] = 'dict'
-				rc['value']['data'] = {}
-				rc['value']['data']['hostType'] = s['host']['type']
-				f.close()
-			except:
-				rc['info'] = 'can\'t get host type'
-				rc['result'] = 'err'
-		elif command == 'shutdown':
-			rc['info'] = 'host is shutdowning ...'
-			if plat=='Windows' or plat.find('CYGWIN'):
-				os.system(r'c:/Windows/System32/shutdown.exe /s /t 3')
-			else:
-				os.system(r'sudo /sbin/halt')
-		elif command == 'restart':
-			print rc
-			rc['info'] = 'host is restarting ...'
-			if plat == 'Windows' or plat.find('CYGWIN'):
-				os.system(r'c:/Windows/System32/shutdown.exe /r /t 3')
-			else:
-				os.system(r'sudo /sbin/reboot')
+    ''' 返回主机类型 
+    '''
+    def get(self):
+        rc = {}
+        rc['result'] = 'ok'
+        rc['info'] = ''
+        command = self.get_argument('command', 'nothing')
+        if command == 'type':
+            try:
+                f = io.open(r'../host/config.json', 'r', encoding='utf-8')
+                s = json.load(f)
+                rc['value'] = {}
+                rc['value']['type'] = 'dict'
+                rc['value']['data'] = {}
+                rc['value']['data']['hostType'] = s['host']['type']
+                f.close()
+            except:
+                rc['info'] = 'can\'t get host type'
+                rc['result'] = 'err'
+        elif command == 'shutdown':
+            rc['info'] = 'host is shutdowning ...'
+            if plat=='Windows' or plat.find('CYGWIN'):
+                os.system(r'c:/Windows/System32/shutdown.exe /s /t 3')
+            else:
+                os.system(r'sudo /sbin/halt')
+        elif command == 'restart':
+            print rc
+            rc['info'] = 'host is restarting ...'
+            if plat == 'Windows' or plat.find('CYGWIN'):
+                os.system(r'c:/Windows/System32/shutdown.exe /r /t 3')
+            else:
+                os.system(r'sudo /sbin/reboot')
 
-		elif command == 'performance':
-			stats = pm.get_all()	
-			rc['info'] = stats					
-		else:
-			rc['info'] = 'can\'t support %s'%(command)
-			rc['result'] = 'err'	
-		self.write(rc)
+        elif command == 'performance':
+            stats = pm.get_all()    
+            rc['info'] = stats                    
+        else:
+            rc['info'] = 'can\'t support %s'%(command)
+            rc['result'] = 'err'    
+        self.write(rc)
 
 def make_app():
-	return Application([
-			url(r'/dm/help', HelpHandler),
-			url(r'/dm/host', HostHandler),
-			url(r'/dm/list', ListServiceHandler),
-			url(r'/dm/([^/]+)/(.*)', ServiceHandler),
-			url(r'/dm/internal', InternalHandler),
-			])
+    return Application([
+            url(r'/dm/help', HelpHandler),
+            url(r'/dm/host', HostHandler),
+            url(r'/dm/list', ListServiceHandler),
+            url(r'/dm/([^/]+)/(.*)', ServiceHandler),
+            url(r'/dm/internal', InternalHandler),
+            ])
 
 import urllib2
 import time
 def get_utf8_body(req):
-	# FIXME: 更合理的应该是解析 Content-Type ...
-	body = '';
-	b = req.read().decode('utf-8')
-	while b:
-		body += b
-		b = req.read().decode('utf-8')
-	return body
+    # FIXME: 更合理的应该是解析 Content-Type ...
+    body = '';
+    b = req.read().decode('utf-8')
+    while b:
+        body += b
+        b = req.read().decode('utf-8')
+    return body
 
 def reg(h_ip, h_mac, h_type, sip, sport):
-	url = 'http://%s:%s/deviceService/regHost?mac=%s&ip=%s&hosttype=%s'%\
-		  (sip, sport, h_mac, h_ip, h_type)
-	try:
-		s = urllib2.urlopen(url)
-	except:
-		return False
-	ret = get_utf8_body(s)
-	if u'ok' in ret:
-		return True
-	else:
-		return False
-		
+    url = 'http://%s:%s/deviceService/regHost?mac=%s&ip=%s&hosttype=%s'%\
+          (sip, sport, h_mac, h_ip, h_type)
+
+    print 'reg: url', url
+    try:
+        s = urllib2.urlopen(url)
+    except:
+        return False
+    ret = get_utf8_body(s)
+    if u'ok' in ret:
+        return True
+    else:
+        return False
+        
 def reg_host():
-	myip = zkutils().myip_real()
-	mymac = zkutils().mymac()
-	host_type = None
-	sip = None
-	sport = None
-	try:
-		f = io.open(r'../host/config.json', 'r', encoding='utf-8')
-		s = json.load(f)
-		host_type = s['host']['type']
-		sip = s['regHbService']['sip']
-		sport = s['regHbService']['sport']
-		f.close()
-	except:
-		rc['info'] = 'can\'t get host info'
-		rc['result'] = 'err'
-	while reg(myip, mymac, host_type, sip, sport) == False:
-		time.sleep(5)
+    myip = _myip
+    mymac = _mac
+    host_type = None
+    sip = None
+    sport = None
+    try:
+        f = io.open(r'../host/config.json', 'r', encoding='utf-8')
+        s = json.load(f)
+        host_type = s['host']['type']
+        sip = s['regHbService']['sip']
+        sport = s['regHbService']['sport']
+        f.close()
+    except:
+        rc['info'] = 'can\'t get host info'
+        rc['result'] = 'err'
+    while reg(myip, mymac, host_type, sip, sport) == False:
+        time.sleep(5)
 
 def isMacList(url):
-	try:
-		s = urllib2.urlopen(url)
-	except:
-		return False
+    print 'isMacList: calling ...'
+    try:
+        s = urllib2.urlopen(url)
+    except:
+        return False
 
-	ret = get_utf8_body(s)
+    ret = get_utf8_body(s)
 
-	if ret is '':
-		return False
-	else:
-		return True
+    if ret is '':
+        return False
+    else:
+        return True
 
-	
+    
 import threading
 
 class RegHost(threading.Thread):
-	def __init__(self):
-		#FIXME:暂时不采用非友好退出,以后改正 ...
-		self.isQuit = False
-		#TODO:应该采用 event, wait,下一步再添加吧 ...
+    def __init__(self):
+        #FIXME:暂时不采用非友好退出,以后改正 ...
+        self.isQuit = False
+        #TODO:应该采用 event, wait,下一步再添加吧 ...
+        threading.Thread.__init__(self)
 
-	def join(self):
-		self.isQuit = True
+    def join(self):
+        self.isQuit = True
 
-	def run(self):
-		myip = zkutils().myip_real()
-		mymac = zkutils().mymac()
-		host_type = None
-		sip = None
-		sport = None
-		listByMacUrl = None
-		try:
-			f = io.open(r'../host/config.json', 'r', encoding='utf-8')
-			s = json.load(f)
-			host_type = s['host']['type']
-			sip = s['regHbService']['sip']
-			sport = s['regHbService']['sport']
-			f.close()
-		except:
-			rc['info'] = 'can\'t get host info'
-			rc['result'] = 'err'
-		listByMacUrl = r'http://%s:%s/deviceService/listByMac?Mac=%s'%(self.sip, self.sport, self.mymac)
+    def run(self):
+        myip = _myip
+        mymac = _mac
+        host_type = None
+        sip = None
+        sport = None
+        listByMacUrl = None
+        try:
+            f = io.open(r'../host/config.json', 'r', encoding='utf-8')
+            s = json.load(f)
+            host_type = s['host']['type']
+            sip = s['regHbService']['sip']
+            sport = s['regHbService']['sport']
+            f.close()
+        except:
+            rc['info'] = 'can\'t get host info'
+            rc['result'] = 'err'
+        listByMacUrl = r'http://%s:%s/deviceService/listByMac?Mac=%s'%(sip, sport, mymac)
 
 
-		while True:
-			while reg(myip, mymac, host_type, sip, sport) == False:
-				time.sleep(5)
+        while True:
+            while reg(myip, mymac, host_type, sip, sport) == False:
+                time.sleep(5)
 
-			time.sleep(10)
+            time.sleep(10)
 
-			while isMacList(listByMacUrl) == True:
-				time.sleep(10)
-				
-	
+            while isMacList(listByMacUrl) == True:
+                time.sleep(10)
+                
+    
 
 
 if __name__ == '__main__':
 
-rgHost =  RegHost()
-regHost.start()
+    rgHost =  RegHost()
+    rgHost.start()
 
-	# 服务管理器，何时 close ??
-	_sm = ServicesManager.ServicesManager()
-	app = make_app()
-	app.listen(DMS_PORT)
-	_ioloop.start()
+    # 服务管理器，何时 close ??
+    _sm = ServicesManager.ServicesManager(_myip, _myip)
+    app = make_app()
+    app.listen(DMS_PORT)
+    _ioloop.start()
 
-	# 此时，必定执行了 internal?command=exit，可以执行销毁 ...
-	print 'DM end ....'
-	_sm.close()
+    # 此时，必定执行了 internal?command=exit，可以执行销毁 ...
+    print 'DM end ....'
+    _sm.close()
 
