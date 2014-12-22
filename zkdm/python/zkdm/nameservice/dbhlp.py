c95514404d24b6ccdc84a090d94a633a1fd7fd7c
sunkw <sunkwei@gmail.com>
2014-12-22 20:15:30 +0800

    ns: 名字服务的第一个版本：    注册支持：        reghost, regservice, heartbeat    查询支持：        getAllServices

--- zkdm/python/zkdm/nameservice/dbhlp.py
+++ zkdm/python/zkdm/nameservice/dbhlp.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 import sqlite3
-import time
+import time, sys, threading
 
 
 DBNAME = 'ns.db'
@@ -13,100 +13,88 @@ TAB_STATES = 'states'
 S0 = r'create table log (id integer primary key, project varchar(64), level integer, stamp integer, content text)' 
 S1 = r'create table hosts (name varchar(128), type varchar(128))'
 S2 = r'create table services (host varchar(128), name varchar(128), type varchar(128), url varchar(255))'
-S3 = r'create table state (host varchar(128), name varchar(128), type varchar(128), stamp integer)'
+S3 = r'create table states (host varchar(128), name varchar(128), type varchar(128), stamp integer)'
+
+TABS = [ { "name": TAB_HOSTS, "create": S1 },
+         { "name": TAB_SERVICES, "create": S2 },
+         { "name": TAB_STATES, "create": S3 }
+       ]
 
 
 class DBHlp:
-	''' 封装到 sqlite3 的读写 '''
-	def __init__(self):
-		self.__conn = None
-		pass
+    ''' 封装到 sqlite3 的读写 '''
+    def __init__(self):
+        self.__conn = None
+        pass
 
 
-	def __db_open(self):
-		self.__conn = sqlite3.connect(DBNAME)
-		return self.__conn.cursor()
+    def db_open(self):
+        self.__conn = sqlite3.connect(DBNAME)
+        return self.__conn.cursor()
 
+    def db_close(self):
+        self.__conn.commit()
+        self.__conn.close()
+        self.__conn = None
 
-	def __db_close(self):
-		self.__conn.commit()
-		self.__conn.close()
-		self.__conn = None
-
-    def execute(self, sent):
+    def execute(self, c, sent):
         ''' 直接执行 sql 语句，没有返回 '''
-        pass
+        c.execute(sent)
 
-    def query(self, sent):
+    def query(self, c, sent):
         ''' 执行查询，返回 [] '''
-        pass
+        rc = []
+        for rec in c.execute(sent):
+            rc.append(rec)
+        return rc
 
 
-	def __save(self, project, level, stamp, content):
-		c = self.__db_open()
-		s0 = r'insert into log (project, level, stamp, content) values ("{}",{},{},"{}")'.format(project, level, stamp, content)
-		c.execute(s0)
-		self.__db_close()
+class FlushState(threading.Thread):
+    ''' 每隔30秒，删除超时的 services '''
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.daemon = True
+        self.start()
 
+    def run(self):
+        while True:
+            time.sleep(30)
+            self.__flush()
 
-	def __query(self, project = None, level = None, stamp_begin = None, stamp_end = None):
-		''' 查询：总是返回数组
-			[
-			 	{ item ...}, { item ... } ...
-			 ]
-		'''
-		if not stamp_begin:
-			stamp_begin = 0
-		if not stamp_end:
-			stamp_end = time.time()
-
-		rc = []
-		c = self.__db_open()
-		s0 = r'select * from log where stamp > {} and stamp < {}'.format(stamp_begin, stamp_end)
-		if project:
-			s0 += r' and project="{}"'.format(project)
-		if level is not None:
-			s0 += r' and level>={}'.format(level)
-
-
-		for item in c.execute(s0):
-			it = {}
-			it['project'] = item[1]
-			it['level'] = item[2]
-			it['stamp'] = item[3]
-			it['content'] = item[4]
-			rc.append(it)
-		
-		self.__db_close()
-		return rc
-		
+    def __flush(self):
+        s = 'delete from states where stamp < %d' % (time.time() - 30)
+        db = DBHlp()
+        c = db.db_open()
+        db.execute(c, s)
+        db.db_close()
 
 
 def __check_init():
-	''' 检查数据库是否准备好 '''
-	conn = sqlite3.connect(DBNAME)
-	c = conn.cursor()
-	# 检查 log 表是否存在 ...
-	s1 = 'select COUNT(*) from sqlite_master where name="' + TABLENAME + '"'
-	v = c.execute(s1)
+    ''' 检查数据库是否准备好 '''
+    conn = sqlite3.connect(DBNAME)
+    c = conn.cursor()
 
-	exist = (c.execute(s1).next()[0] != 0)
-	if not exist:
-		print 'to init db ...'
-		c.execute(S0)
-	else:
-		print TABLENAME, 'existed!'
-	conn.close
+    for chk in TABS:
+        s1 = 'select COUNT(*) from sqlite_master where name="' + chk['name'] + '"'
+        n = c.execute(s1)
+        found = False
+        for x in n:
+            if int(x[0]) == 1:
+                found = True
+                break
+
+        if not found:
+            print 'INFO: dbhlp: init:', chk['name']
+            c.execute(chk['create'])
+        else:
+            print 'INFO: dbhlp: existed:', chk['name']
+    conn.close
+
+    # 启动工作线程，每隔30秒，刷新一次 states 表
+    th = FlushState()
 
 
 # 保证数据结构 ...
 __check_init()
 
 
-if __name__ == '__main__':
-	print 'testing'
-
-	db = DBHlp()
-	db.save('test me', 99, time.time(), r"{'key':22, 'code':-102, 'descr':'cant open file'}")
-	r = db.query()
-	print r
--- zkdm/python/zkdm/nameservice/query.py
+++ zkdm/python/zkdm/nameservice/query.py
@@ -10,9 +10,32 @@
 from dbhlp import DBHlp
 
 
-def getAllServices(offline = False):
+def getAllServices(params):
     ''' 返回服务列表 '''
-    return { 'result': 'ok', 'info': '', 'value':[] }
+    offline = False
+    if 'offline' in params and params['offline'] == '1':
+        offline = True
+
+    s = 'select * from services'
+    if not offline:
+        s = 'select services.host, services.name, services.type, services.url from services join \
+            states on (services.host=states.host and services.name=states.name and \
+                    services.type=states.type)'
+    db = DBHlp()
+    c = db.db_open()
+    rc = db.query(c, s)
+
+    value = []
+    for rec in rc:
+        item = {}
+        item['host'] = rec[0]
+        item['name'] = rec[1]
+        item['type'] = rec[2]
+        item['url'] = rec[3]
+        value.append(item)
+
+    db.db_close()
+    return { 'result': 'ok', 'info': '', 'value':value }
 
 
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
