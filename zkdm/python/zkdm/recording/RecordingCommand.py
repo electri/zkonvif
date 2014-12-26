9f1895f97d9d225dc3c2456e6b35eb23babf2b36
wangze <wangze@zonekey.com.cn>
2014-12-26 16:01:38 +0800

    ..

--- zkdm/python/zkdm/recording/RecordingCommand.py
+++ zkdm/python/zkdm/recording/RecordingCommand.py
@@ -10,13 +10,14 @@ class RecordingCommand():
         pass
 	
     #录像程序的命令接收端口号固定为1230
-    def send_command(self,command):
+    def send_command(self,command,ip,hosttype):
         rc={}
         rc['result']='ok'
         rc['info']=''
         try:
             s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            host='127.0.0.1'
+            #host='192.168.12.47'
+            host = ip
             port=1230
             s.connect((host,port))
             #command = command.encode('utf-8')
@@ -32,25 +33,25 @@ class RecordingCommand():
             rc['info']=str(err)
             return rc
 
-    def preview(self):
+    def preview(self,ip,hosttype):
         rc={}
         rc['result']='ok'
-        ip = self.send_command('BroadCastCmd=GetDeviceIP')
-        if(ip['result'] == 'ok' and len(ip['info'])>0):            
-            ip = ip['info']
-            ip = ip[:-2]
+        rtsp_ip = self.send_command('BroadCastCmd=GetDeviceIP')
+        if(rtsp_ip['result'] == 'ok' and len(rtsp_ip['info'])>0):            
+            rtsp_ip = rtsp_ip['info']
+            rtsp_ip = rtsp_ip[:-2]
             url = {}
-            url['resource1'] = 'rtsp://root:root@'+ ip +':554/session0.mpg'
-            url['resource2'] = 'rtsp://root:root@'+ ip +':554/session1.mpg'
-            url['resource3'] = 'rtsp://root:root@'+ ip +':554/session2.mpg'
-            url['resource4'] = 'rtsp://root:root@'+ ip +':554/session3.mpg'
-            url['resource5'] = 'rtsp://root:root@'+ ip +':554/session4.mpg'
-            url['resource6'] = 'rtsp://root:root@'+ ip +':554/session5.mpg'
-            url['movie'] = 'rtsp://root:root@'+ ip +':554/session6.mpg'
+            url['resource1'] = 'rtsp://root:root@'+ rtsp_ip +':554/session0.mpg'
+            url['resource2'] = 'rtsp://root:root@'+ rtsp_ip +':554/session1.mpg'
+            url['resource3'] = 'rtsp://root:root@'+ rtsp_ip +':554/session2.mpg'
+            url['resource4'] = 'rtsp://root:root@'+ rtsp_ip +':554/session3.mpg'
+            url['resource5'] = 'rtsp://root:root@'+ rtsp_ip +':554/session4.mpg'
+            url['resource6'] = 'rtsp://root:root@'+ rtsp_ip +':554/session5.mpg'
+            url['movie'] = 'rtsp://root:root@'+ rtsp_ip +':554/session6.mpg'
             rc['info'] = url
         else:
             rc['result'] = 'error'
-            rc['info'] = ip['info']
+            rc['info'] = rtsp_ip['info']
         
         return rc
 
