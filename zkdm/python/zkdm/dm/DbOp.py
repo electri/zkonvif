99f565bd2561dda4b6ee026d4f974165edc2c966
sunkw <sunkwei@gmail.com>
2014-12-31 10:21:07 +0800

    dm: DbOp.py 增加 _init_db_from_tokens()，用于初始化数据库，从 tokens.json 中读取数据，初始化表

--- zkdm/python/zkdm/dm/DbOp.py
+++ zkdm/python/zkdm/dm/DbOp.py
@@ -5,7 +5,11 @@
 # @detail:
 #
 #################################################################
-import sqlite3
+import sqlite3, sys
+sys.path.append('../')
+from common.uty_token import *
+
+DB_FNAME = 'proxied_hosts.db'
 
 # XXX: 下面对 sql 语句的使用，可能有问题,
 #      sql 语句中的 char/vchar 类型，必须使用 ""，如
@@ -48,7 +52,53 @@ class DbOp:
     def close(self):
         self.conn.close()
 
+
+
+
+def _db_init():
+    ''' 检查数据库, 如果 hosts_state 表不存在，则创建之
+    '''
+    db = sqlite3.connect(DB_FNAME)
+    c = db.cursor()
+    s0 = 'select COUNT(*) from sqlite_master where name="hosts_state"'
+    rc = c.execute(s0)
+    if int(rc.next()[0]) == 0:
+        s1 = 'create table hosts_state(ip varchar(128), isLive integer)'
+        c.execute(s1)
+    db.commit()
+    db.close()
+
+
+def _db_init_from_tokens(fname):
+    ''' fname 为 tokens.json，从 tokens.json 中提取被代理主机信息，然后
+        更新 hosts_state 表，初始 isLive 都设置为 0
+    '''
+    _db_init()
+    hosts = gather_hds(fname)
+    ips = []
+    for host in hosts:
+        if host['tokenid'] == "0": # 不加载本机
+            continue
+        if 'ip' in host:
+            ips.append(host['ip'])
+
+    db = sqlite3.connect(DB_FNAME)
+    c = db.cursor()
+    s0 = 'delete from hosts_state'
+    c.execute(s0)
+
+    for ip in ips:
+        s1 = 'insert into hosts_state values("%s", 0)' % (ip)
+        c.execute(s1)
+
+    db.commit()
+    db.close()
+
+
 if __name__ == '__main__':
+    _db_init_from_tokens('../common/tokens.json')
+    sys.exit()
+
     db = DbOp('proxied_hosts.db')
     db.alterValue('hosts_state', ('172.16.1.10', 1))
     t = db.selectByOpt('hosts_state', "isLive = '1'") 
