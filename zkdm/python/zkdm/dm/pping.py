9561c76c457a61648c0cb0e6fcdf12262c35cdbe
sunkw <sunkwei@gmail.com>
2014-12-31 11:38:38 +0800

    dm: 简单的 ping

---
+++ zkdm/python/zkdm/dm/pping.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+# coding: utf-8
+#
+# @file: pping.py
+# @date: 2014-12-31
+# @brief:
+# @detail:
+#
+#################################################################
+
+
+from DbOp import *
+import socket
+sys.path.append('../common')
+import uty_token
+import select, time
+
+
+TARGET_PORT = 11011
+
+
+def _send_pings(fd, ips):
+    for ip in ips:
+        print 'send ping to:', ip
+        fd.sendto('ping', (ip, TARGET_PORT))
+
+
+def ping_all(fname):
+    ''' fname 为 tokens.json '''
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ips = db_init_from_tokens(fname)
+
+    last_send_ping = 0
+    while True:
+        rc = select.select([sock], [], [], 10)
+        print 'select ret:', rc
+
+        if len(rc[0]) == 0: # timeout, just 
+            _send_pings(sock, ips)
+            db_check_timeout(time.time())
+            last_send_ping = time.time()
+
+        elif len(rc[0]) > 0: # 能收到 pong 了
+            pong, remote = sock.recvfrom(16)
+            print pong, remote
+            if pong == 'pong':
+                remote_ip = remote[0]
+                db_update(remote_ip)
+
+            t = time.time()
+            if t - last_send_ping > 10:
+                _send_pings(sock, ips)
+                db_check_timeout(t)
+                last_send_ping = t
+
+
+if __name__ == '__main__':
+    ping_all('../common/tokens.json')
+
+
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+
