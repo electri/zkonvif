3e2b1b42a150913387683cea3ae2738469272db2
wangze <wangze@zonekey.com.cn>
2015-01-15 15:36:32 +0800

    ..

--- zkdm/python/zkdm/recording/DiskManagement.py
+++ zkdm/python/zkdm/recording/DiskManagement.py
@@ -43,7 +43,7 @@ def dir_list_file(path = 'C:\RecordFile'):
     return dir_list
 
 def del_dir_schedule():
-    thread = threading.Timer(2*3600,del_dir) #2小时执行一次
+    thread = threading.Timer(2*3600,del_dir_schedule) #2小时执行一次
     thread.start()
     if get_fs_info():
         dir_list = dir_list_file()
@@ -55,3 +55,4 @@ def del_dir_schedule():
             except Exception as error:
                 print error
             dir_list.remove(dir_list[0])
+
