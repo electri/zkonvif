e11668903725bf9f3661675963000cbe8e76a12a
wangze <wangze@zonekey.com.cn>
2015-01-22 14:28:49 +0800

    ..

--- zkdm/python/zkdm/recording/ClassSchedule.py
+++ zkdm/python/zkdm/recording/ClassSchedule.py
@@ -184,6 +184,18 @@ class Schedule():
         except Exception as error:
             print str(error)
 
+    
+
+    def set_recording_dir(self,ip='127.0.0.1',hosttype='x86'):
+        set_thread = threading.Timer(600, self.set_recording_dir)
+        set_thread.start()
+        try:
+            _rcmd = RecordingCommand()
+            _rcmd.send_command('RecordCmd=SetFileProperty&FileFormat=mp4&TotalFilePath=D:/RecordFile')
+
+        except Exception as error:
+            print str(error)
+
     def analyse_json(self,ip='127.0.0.1',hosttype='x86'):
         reload_thread = threading.Timer(3600, self.analyse_json)#1小时重新获取一次课表信息
         reload_thread.start()
