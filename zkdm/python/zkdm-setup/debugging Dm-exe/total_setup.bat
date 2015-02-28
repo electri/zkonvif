b0413f93e51efa50470c85223833bda73a9d9dfb
liuguangyuan <liuguangyuan0532@gmail.com>
2015-02-28 18:12:44 +0800

    ..

--- zkdm/python/zkdm-setup/debugging Dm-exe/total_setup.bat
+++ zkdm/python/zkdm-setup/debugging Dm-exe/total_setup.bat
@@ -19,13 +19,13 @@ vcredist_x86.exe
 
 
 
-@if exist c:\zkdm\ptz\teacher.config (copy c:\zkdm\ptz\teacher.config .\zkdm\ptz\teacher.config /Y)
+@if exist c:\zkdm\ptz\card0.config (copy c:\zkdm\ptz\card0.config .\zkdm\ptz\card0.config /Y)
 
 
-@if exist c:\zkdm\ptz\student.config (copy c:\zkdm\ptz\student.config .\zkdm\ptz\student.config /Y)
+@if exist c:\zkdm\ptz\card2.config (copy c:\zkdm\ptz\card2.config .\zkdm\ptz\card2.config /Y)
 
 
-@if exist c:\zkdm\ptz\bd.config (copy c:\zkdm\ptz\bd.config .\zkdm\ptz\bd.config /Y)
+@if exist c:\zkdm\ptz\card5.config (copy c:\zkdm\ptz\card5.config .\zkdm\ptz\card5.config /Y)
 
 
 @if exist c:\zkdm\host\config.json (copy c:\zkdm\host\config.json .\zkdm\host\config.json /Y)
@@ -35,13 +35,13 @@ vcredist_x86.exe
 
 @cd c:\zkdm\ptz
 @echo please alter teacher.config
-@notepad card1.config
+@notepad card0.config
 
 @echo please alter student.config
 @notepad card2.config
 
 @echo please alter bd
-@notepad card0.config
+@notepad card5.config
 @cd c:\zkdm\host
 
 @echo please alter host type and reg service parameters
