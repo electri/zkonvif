20cfb838c3ec078618985b462b59cb33b1677fdf
yangyifneg <yangyifeng>
2015-03-06 11:27:48 +0800

    更新日志追加，大于5M删除

--- zkdm/python/zkdm/autoupdate/checkVersion.py
+++ zkdm/python/zkdm/autoupdate/checkVersion.py
@@ -12,12 +12,15 @@ from conf_mc import *
 #self.removte_zip_version = "1.0"
 class CheckVersion:
 	def __init__(self):
+		#日志大于5M，删除
+		if os.path.getsize("update.log") > 5000000:
+			os.remove("update.log")
 		#日志格式
 		logging.basicConfig(level=logging.DEBUG,
                 format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                 datefmt='%a, %d %b %Y %H:%M:%S',
                 filename='update.log',
-                filemode='w')
+                filemode='a')
 
 		#################################################################################################
 		#定义一个StreamHandler，将INFO级别或更高的日志信息打印到标准错误，并将其添加到当前的日志处理对象#
@@ -27,7 +30,7 @@ class CheckVersion:
 		console.setFormatter(formatter)
 		logging.getLogger('').addHandler(console)
 		#################################################################################################
-
+		logging.debug("#################################################################################################")
 		#获得更新url
 		conf = getconf_mulcast()
 		c = json.loads(conf)
@@ -47,39 +50,46 @@ class CheckVersion:
 		self.zip_file_name = "text.zip"
 		self.local_version_config = {}
 		self.remote_version_config = {}
-		self.loadfile(self.version_local_file_name, self.local_version_config)
-		logging.debug("开始下载文件%s"%self.zip_file_name)
+
+
 
 
 	def download_file(self, remote_file_url = "",  local_file_name = ""):
 		try:
-
 			file_version = urlopen(remote_file_url)
 			data = file_version.read()
 			print len(data)
+			with open(local_file_name, "wb") as local_zip:
+				local_zip.write(data)
+			return True
+
 		except Exception, ex:
 			logging.debug(ex)
-			sys.exit(0)
+			return False
 
-		with open(local_file_name, "wb") as local_zip:
-			local_zip.write(data)
+
 
 
 
 	def loadfile(self, file_name, config):
-		with open(file_name, "r") as f:
+		try:
+			f = open(file_name, "r")
 			for line in f.readlines():
 				line = line.strip("\n")
 				arr = line.split("=",2)
 				if len(arr) == 2:
 					config[arr[0].strip()] = arr[1].strip()
+			return True
+		except Exception,ex:
+			logging.debug(ex)
+			return False
 
 	def savefile(self, file_name, config):
 		with open(file_name, "wb") as f:
 			for key in config:
 				f.write(key + '=' + config[key] + '\n')
 
-
+	#py文件最后解压，version和local不解压
 	def extract_zip(self, zip_file_name, folder = ""):
 		try:
 			lastfile = None
@@ -88,39 +98,49 @@ class CheckVersion:
 				if file.find("checkVersion.py") != -1:
 					lastfile = file
 					continue
+				if file.find("autoupdate/version")!=-1 or file.find("autoupdate/local")!= -1 or file.find("autoupdate/update.log") != -1:
+					continue
 				f.extract(file, folder)
 			if lastfile != None:
 				f.extract(lastfile, folder)
 			f.close()
+			return True
 		except Exception,ex:
 			logging.debug(ex)
-			sys.exit(0)
+			return False
 
 	def checkVersionProcess(self):
+		self.loadfile(self.version_local_file_name, self.local_version_config)
+
 		bZipModify = False
 		logging.debug("下载version文件")
-		self.download_file(self.version_url, self.version_file_name)
+		if self.download_file(self.version_url, self.version_file_name) == False:
+			return False
 
 		logging.debug("加载version文件，进行比对")
-		self.loadfile(self.version_file_name, self.remote_version_config)
+		if self.loadfile(self.version_file_name, self.remote_version_config) == False:
+			return False
 
 		logging.debug("检查py文件是否有更新")
-		if self.local_version_config["py_version"] != self.remote_version_config["py_version"]:
+		if self.local_version_config.get("py_version") != self.remote_version_config.get("py_version"):
 			logging.debug("更新py文件")
 			self.download_file(self.py_url, self.py_file_name)
-			self.local_version_config["py_version"] = self.remote_version_config["py_version"]
+			self.local_version_config["py_version"] = self.remote_version_config.get("py_version")
 
 			self.savefile(self.version_local_file_name, self.local_version_config)
 
 			return False
 
 		logging.debug("检查zip文件是否有更新")
-		if self.local_version_config["zip_version"] != self.remote_version_config["zip_version"]:
+		if self.local_version_config.get("zip_version") != self.remote_version_config.get("zip_version"):
 			logging.debug("更新zip文件")
-			self.download_file(self.zip_url, self.zip_file_name)
+			if self.download_file(self.zip_url, self.zip_file_name) == False:
+				return False
+
 			logging.debug("解压zip文件")
-			self.extract_zip(self.zip_file_name, "../")
-			self.local_version_config["zip_version"] = self.remote_version_config["zip_version"]
+			if self.extract_zip(self.zip_file_name, "../") == False:
+				return False
+			self.local_version_config["zip_version"] = self.remote_version_config.get("zip_version")
 			bZipModify = True
 
 
