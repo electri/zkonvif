df83fb68af69a99efaeb14096de4cbe359b37153
liuguangyuan <liuguangyuan0532@gmail.com>
2015-03-10 14:52:38 +0800

    ..

--- zkdm/python/zkdm/ptz/PtzServer.py
+++ zkdm/python/zkdm/ptz/PtzServer.py
@@ -24,11 +24,12 @@ except:
     print 'only one instance can be run!!!'
     sys.exit(0)
 
-_all_config = None
+_all_config = json.load(io.open('./config.json', 'r', encoding='utf-8'))
+_local_config = None
 if os.path.isfile('./local.json'):
-    _all_config = json.load(io.open('./local.json', 'r', encoding='utf-8'))
-else:
-    _all_config = json.load(io.open('./config.json', 'r', encoding='utf-8'))
+    _local_config = json.load(io.open('./local.json', 'r', encoding='utf-8'))
+    _all_config.update(_local_config)
+
 _tokens = load_tokens('../common/tokens.json')
 logging.basicConfig(filename='ptz.log', filemode='w', level=logging.DEBUG)
 def all_ptzs_config():
